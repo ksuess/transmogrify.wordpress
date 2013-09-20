d34dcff1726a11ec3f3cdff813624d392c0f9dce
cewing <cris@crisewing.com>
2013-09-19 17:34:44 -0700

    * fix wordpress namespace version (should this be declarable)? * add workaround for nasty lxml bug

--- transmogrify/wordpress/blueprints.py
+++ transmogrify/wordpress/blueprints.py
@@ -12,7 +12,8 @@ from DateTime import DateTime
 from zope.component import createObject
 from plone.app.discussion.interfaces import IConversation
 from Products.CMFPlone.utils import safe_unicode
-from Products.TinyMCE.transforms.parser import TinyMCEOutput, singleton_tags
+# from Products.TinyMCE.transforms.parser import TinyMCEOutput, singleton_tags
+from plone.outputfilters.filters.resolveuid_and_caption import ResolveUIDAndCaptionFilter
 
 from StringIO import StringIO
 from OFS.Image import File
@@ -23,7 +24,7 @@ logger = logging.getLogger('transmogrify.wordpress')
 # XML namespaces
 CONTENT = '{http://purl.org/rss/1.0/modules/content/}'
 DC = '{http://purl.org/dc/elements/1.1/}'
-WP = '{http://wordpress.org/export/1.0/}'
+WP = '{http://wordpress.org/export/1.2/}'
 
 
 class WXRSource(object):
@@ -42,10 +43,19 @@ class WXRSource(object):
 
         file = open(self.filename, 'rb')
         i = 0
-        for event, node in etree.iterparse(file, tag='item'):
+        for event, node in etree.iterparse(self.filename):
+            # workaround for bug in lxml < 3.2.2
+            # (see https://bugs.launchpad.net/lxml/+bug/1185701)
+            if node.tag != 'item':
+                if node.getparent() is None:
+                    import pdb; pdb.set_trace( )
+                    break
+                else:
+                    continue
+
             i += 1
             item = dict()
-            
+
             item['portal_type']   = self.portal_type
             item_id               = node.findtext(WP + 'post_name')
             # Zope ids need to be ASCII
@@ -54,34 +64,34 @@ class WXRSource(object):
             item['_path']         = path
             item['_orig_url']     = node.findtext('link')
             logger.info('Importing %s' % path)
-            
+
             item['title']         = node.findtext('title')
             item['description']   = node.findtext('description')
             item['creation_date'] = node.findtext('pubDate')
             item['effectiveDate'] = item['creation_date']
             item['modification_date'] = item['creation_date']
             item['creators']      = [node.findtext(DC + 'creator')]
-            
+
             tags = set([])
             for category in node.iterfind('category'):
                 tags.add(category.text)
             item['subject']       = sorted(tags)
-            
+
             item['text']          = node.findtext(CONTENT + 'encoded')
-            
+
             status                = node.findtext(WP + 'status')
             if status == 'publish':
                 item['_transitions'] = 'publish'
 
             yield item
-            
+
             # comments
             by_comment_id = lambda x: x.findtext(WP + 'comment_id')
             for node in sorted(node.iterfind(WP + 'comment'), key=by_comment_id):
                 # skip spam/unapproved comments
                 if node.findtext(WP + 'comment_approved') != '1':
                     continue
-                
+
                 text = safe_unicode(node.findtext(WP + 'comment_content'))
                 author_name = node.findtext(WP + 'comment_author')
                 # add link to pingbacks
@@ -89,7 +99,7 @@ class WXRSource(object):
                     url = node.findtext(WP + 'comment_author_url')
                     text = u'<a href="%s">%s</a>' % (url, text)
                     author_name = None
-                
+
                 item = dict()
                 item['portal_type']  = 'plone.Comment'
                 item['_path']        = path # path to parent object
@@ -99,15 +109,17 @@ class WXRSource(object):
                 item['author_email'] = node.findtext(WP + 'comment_author_email')
                 item['created']      = node.findtext(WP + 'comment_date')
                 item['text']         = node.findtext(WP + 'comment_content')
-                
+
                 yield item
-            
+
             # release memory
             node.clear()
-            
+
             # if i > MAX:
             #     break
-            
+        except Exception, e:
+            import pdb; pdb.set_trace( )
+
         file.close()
 
 
@@ -136,7 +148,7 @@ class WordpressTextCleanupSection(object):
         text = self.PRE_RE.sub(lambda x: x.group(1).replace('\r\n\r\n', '\n\n'), text)
         text = text.replace('\r\n\r\n', '<p>').replace('\r\n','\n').replace('\n', '<br />\n')
         return text
-        
+
         # TODO: handle [googlevideo] links, [gallery], [caption], others?
 
 
@@ -178,16 +190,16 @@ class HTMLFetcher(object):
             yield item
 
 
-class ImageMungingParser(TinyMCEOutput):
-    
+class ImageMungingParser(ResolveUIDAndCaptionFilter):
+
     def __init__(self, site_path, base_path='/images'):
-        TinyMCEOutput.__init__(self)
+        ResolveUIDAndCaptionFilter.__init__(self)
         self.atag = None
         self.abuffer = []
         self.items = []
         self.site_path = site_path
         self.base_path = base_path
-    
+
     def append_data(self, data, add_eol=0):
         if add_eol:
             data += '\n'
@@ -195,13 +207,13 @@ class ImageMungingParser(TinyMCEOutput):
             self.abuffer.append(data)
         else:
             self.pieces.append(data)
-    
+
     def unknown_starttag(self, tag, attrs):
         if tag in ['a', 'img']:
             attributes = {}
             for (key, value) in attrs:
                 attributes[key] = value
-        
+
             if tag == 'a':
                 self.atag = attributes
             elif tag == 'img' and 'src' in attributes:
@@ -222,7 +234,7 @@ class ImageMungingParser(TinyMCEOutput):
                     res = safe_urlopen(src)
                     if res is not None:
                         mimetype = res.info()['content-type']
-                
+
                 if res is not None:
                     scheme, host, path, query, frag = urlsplit(src)
                     filename = path.split('/')[-1]
@@ -230,23 +242,23 @@ class ImageMungingParser(TinyMCEOutput):
                     filename = unquote_plus(filename).decode('utf8').encode('ascii', 'ignore')
                     # wrap the data so it'll get added with the correct filename & mimetype
                     data = File(filename, 'image', StringIO(res.read()), mimetype)
-            
+
                     # XXX choose scale size
-            
+
                     # prepare image info for injection to transmogrifier pipeline
-            
+
                     item = dict()
-            
+
                     item['portal_type']   = 'Image'
                     path = '/'.join([self.base_path, filename])
                     # XXX avoid collisions
                     item['_path']         = path
                     logger.info('Importing %s' % path)
-            
+
                     item['image']         = data
-            
+
                     self.items.append(item)
-                    
+
                     # update src attribute
                     attributes['src'] = '/'.join([self.site_path, path])
                     if self.atag and 'href' in self.atag:
@@ -254,12 +266,12 @@ class ImageMungingParser(TinyMCEOutput):
                 else:
                     # couldn't fetch image; leave src as is
                     logger.warn("Couldn't fetch image: %s" % src)
-            
+
             attrs = attributes.iteritems()
 
         if tag != 'a':
             self.append_tag(tag, attrs)
-    
+
     def unknown_endtag(self, tag):
         if tag == 'a':
             attrs = self.atag.items()
@@ -267,11 +279,11 @@ class ImageMungingParser(TinyMCEOutput):
             self.append_tag('a', attrs)
             self.pieces.extend(self.abuffer)
             self.abuffer = []
-        return TinyMCEOutput.unknown_endtag(self, tag)
+        return ResolveUIDAndCaptionFilter.unknown_endtag(self, tag)
 
     def append_tag(self, tag, attrs):
         strattrs = ''.join([' %s="%s"' % (key, value) for key, value in attrs])
-        if tag in singleton_tags:
+        if tag in self.singleton_tags:
             self.append_data('<%s%s />' % (tag, strattrs))
         else:
             self.append_data('<%s%s>' % (tag, strattrs))
@@ -284,7 +296,7 @@ class HTMLImageSource(object):
     """
     classProvides(ISectionBlueprint)
     implements(ISection)
-    
+
     def __init__(self, transmogrifier, name, options, previous):
         self.options = options
         self.previous = previous
@@ -305,7 +317,7 @@ class HTMLImageSource(object):
                 images = parser.items
 
             yield item
-            
+
             for item in images:
                 yield item
 
@@ -329,12 +341,12 @@ class MimetypeSetter(object):
 class CommentConstructor(object):
     classProvides(ISectionBlueprint)
     implements(ISection)
-    
+
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.options = options
         self.context = transmogrifier.context
-        self.typekey = defaultMatcher(options, 'type-key', name, 'type', 
+        self.typekey = defaultMatcher(options, 'type-key', name, 'type',
                                       ('portal_type', 'Type'))
         self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
         self.comment_map = {}
